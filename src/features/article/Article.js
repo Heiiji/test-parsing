fd722a487c5a66491a32b46d3f5dbcb166ed0643
Heiji <julien.juret@gmail.com>
2020-06-27 09:39:15 +0200

    [REMOVE] - warning

--- src/features/article/Article.js
+++ src/features/article/Article.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { setList } from "./articlesSlice";
 import api from "../../app/api";
